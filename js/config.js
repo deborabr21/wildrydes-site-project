9ba772cde111cacb5c43f0e68ff7b79a83acda4d
Debbie <72487682+deborabr21@users.noreply.github.com>
2025-10-23 15:13:53 -1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_U0MEYAQrv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7g8vq75hf8l3dg2fk8bvhpg39q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
